68ef4688b4fdff53172d432dba74946f2351cc90
Sourav12061999 <sdunity12061999@gmail.com>
2022-06-16 13:35:02 +0530

    Pages and Navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,19 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ChakraProvider, theme } from "@chakra-ui/react";
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Pages/Home/Home";
+import Students from "./Pages/Students/Students";
+import Error from "./Pages/Error/Error";
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Students" element={<Students />} />
+        <Route path="/*" element={<Error />} />
+      </Routes>
+    </Router>
   </ChakraProvider>
-)
+);
---
+++ src/Components/Navbar/NavItems.ts
@@ -0,0 +1,23 @@
+export type NavItem = {
+    label: string;
+    href?: string;
+  }
+  
+  export const NAV_ITEMS: Array<NavItem> = [
+    {
+      label: "Home",
+      href:"#"
+    },
+    {
+      label: "Students",
+      href:"#"
+    },
+    {
+      label: "Active",
+      href: "#",
+    },
+    {
+      label: "Done",
+      href: "#",
+    },
+  ];
