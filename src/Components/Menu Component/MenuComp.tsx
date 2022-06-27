c03c5389783fb2a1550c53cc0dc15d8d70fa3e53
Sourav12061999 <sdunity12061999@gmail.com>
2022-06-27 09:16:59 +0530

    Changed Runscript from menu to button

--- src/Components/Menu Component/MenuComp.tsx
+++ src/Components/Menu Component/MenuComp.tsx
@@ -1,37 +1,58 @@
-import React from 'react'
-import {Menu,MenuButton,MenuList,MenuItem,Button} from "@chakra-ui/react";
-import {ChevronDownIcon} from "@chakra-ui/icons";
-import {TableRow} from "../Table/table.types";
+import React from "react";
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button,
+  Box,
+} from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import { TableRow } from "../Table/table.types";
 import useAddToColab from "../../Hooks/useAddToColab";
-interface Props{
-  tableRows: Array<TableRow>,
-  isActivePage?:boolean,
-  fetchStudentsData:Function,
+interface Props {
+  tableRows: Array<TableRow>;
+  isActivePage?: boolean;
+  fetchStudentsData: Function;
 }
-function MenuComp({tableRows,isActivePage,fetchStudentsData}:Props) {
-  const AddToColab=useAddToColab();
+function MenuComp({ tableRows, isActivePage, fetchStudentsData }: Props) {
+  const AddToColab = useAddToColab();
   return (
-  <Menu>
-  <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-    Menu
-  </MenuButton>
-  <MenuList>
-    <MenuItem onClick={() =>{
-      console.log(tableRows)
-    }}>Delete</MenuItem>
-    <MenuItem>Download</MenuItem>
-    {
-      isActivePage && (<MenuItem onClick={ async ()=>{
-        console.log("Ran Script");
-        await AddToColab(tableRows.filter((el) =>{
-          return el.isSelected===true;
-        }))
-        await fetchStudentsData();
-      }}>Run Script</MenuItem>)
-    }
-  </MenuList>
-</Menu>
-  )
+    <Box>
+      <Menu>
+        <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+          Menu
+        </MenuButton>
+        <MenuList>
+          <MenuItem
+            onClick={() => {
+              console.log(tableRows);
+            }}
+          >
+            Delete
+          </MenuItem>
+          <MenuItem>Download</MenuItem>
+        </MenuList>
+      </Menu>
+      {isActivePage && (
+            <Button
+             marginLeft={"1.5"}
+             colorScheme={"blue"}
+              onClick={async () => {
+                console.log("Ran Script");
+                await AddToColab(
+                  tableRows.filter((el) => {
+                    return el.isSelected === true;
+                  })
+                );
+                await fetchStudentsData();
+              }}
+            >
+              Run Script
+            </Button>
+          )}
+    </Box>
+  );
 }
 
-export default MenuComp
+export default MenuComp;
